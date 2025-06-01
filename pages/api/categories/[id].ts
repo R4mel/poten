b316f4775607615f803e697e8d880c4938efbe22
한인혁 <138432939+R4mel@users.noreply.github.com>
2025-06-01 17:59:50 +0900

    fix }

--- pages/api/categories/[id].ts
+++ pages/api/categories/[id].ts
@@ -66,3 +66,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
   res.status(405).end(`Method ${req.method} Not Allowed`);
+}
